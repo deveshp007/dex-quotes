69c0d2252346be4529c415705dc25fc938734ccf
Devesh Pandey <72290650+deveshp007@users.noreply.github.com>
2023-02-02 20:04:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [quote, setQuote] = useState({});
+  const [backgroundColor, setBackgroundColor] = useState("#fff");
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    const res = await fetch("https://api.quotable.io/random");
+    const data = await res.json();
+    setQuote(data);
+    setBackgroundColor(generateRandomColor());
+  };
+
+  const generateRandomColor = () => {
+    const colors = [
+      "#16a085",
+      "#27ae60",
+      "#2980b9",
+      "#8e44ad",
+      "#2c3e50",
+      "#f39c12",
+      "#d35400",
+      "#c0392b"
+    ];
+
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+  document.body.style.backgroundColor = backgroundColor;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: backgroundColor }}>
+      <h1>DeX Quote Machine</h1>
+      <p>"{quote.content}"</p>
+      <span>- {quote.author}</span>
+      <br />
+      <button className="btn" onClick={fetchQuote}>Get New Quote</button>
     </div>
   );
 }
 
 export default App;
+
